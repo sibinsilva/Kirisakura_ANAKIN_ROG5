e2444b62283acee2fe86b3b603dcf3735c32492b
Sibin Silvester <sibinsilva@gmail.com>
2026-06-28 08:25:39 +0000

    tcp: fix signed-integer-overflow in tcp_add_backlog() (manual backport)
    Cast sk_rcvbuf and sk_sndbuf to u32 before addition to prevent signed
    integer overflow. Also shift sndbuf right by 1 to match upstream fix.
    CVE backport from 9d04b4d0feee.

--- net/ipv4/tcp_ipv4.c
+++ net/ipv4/tcp_ipv4.c
@@ -1672,7 +1672,7 @@ int tcp_v4_early_demux(struct sk_buff *skb)
 
 bool tcp_add_backlog(struct sock *sk, struct sk_buff *skb)
 {
-	u32 limit = READ_ONCE(sk->sk_rcvbuf) + READ_ONCE(sk->sk_sndbuf);
+	u32 limit = (u32)READ_ONCE(sk->sk_rcvbuf) + (u32)(READ_ONCE(sk->sk_sndbuf) >> 1);
 	u32 tail_gso_size, tail_gso_segs;
 	struct skb_shared_info *shinfo;
 	const struct tcphdr *th;
